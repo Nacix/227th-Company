24d7c622eb3f106a28f1605038525be751765579
Nacix <anorexicanalt@gmail.com>
2024-01-27 22:01:35 -0500

    fix: missing semicolon

--- addons/rep_armor/XtdGearInfos.hpp
+++ addons/rep_armor/XtdGearInfos.hpp
@@ -234,7 +234,7 @@ class XtdGearInfos
 		XTD_V_OMNI(Kama_Medic,Leadership,Medic);
 		XTD_V_OMNI(Kama_CS,Leadership,Sergeant);
 		XTD_V_OMNI(Kama_Officer,Leadership,Officer);
-		XTD_V_OMNI(Kama_Officer_Geonosis,Leadership,Officer_Geonosis)
+		XTD_V_OMNI(Kama_Officer_Geonosis,Leadership,Officer_Geonosis);
 		XTD_V_OMNI(Kama_Spartacus,Leadership,Spartacus);
 		XTD_V_OMNI(Kama_Valyrian,Leadership,Valyrian);
 
